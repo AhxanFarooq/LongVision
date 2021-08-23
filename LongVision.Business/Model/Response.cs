fa2cfcc89d3bb8883dd17d5f01db5f510dedee68
AhxanFarooq <mahsangee265@outlook.com>
2021-08-23 08:18:23 -0700

    working on identity

---
+++ LongVision.Business/Model/Response.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection.Metadata;
+using System.Text;
+
+namespace LongVision.Business.Model
+{
+    public class Response
+    {
+        public string Status { get; set; }
+        public string Message { get; set; }
+    }
+}
---
+++ LongVision.Persistance/ApplicationUser.cs
@@ -0,0 +1,9 @@
+﻿using System;
+using Microsoft.AspNetCore.Identity;
+
+namespace LongVision.Persistent
+{
+    public class ApplicationUser:IdentityUser
+    {
+    }
+}
